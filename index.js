f2010b901741cf3780526bdccb8720db66f3cbc2
Alex Cleveland <Alexcleveland5@gmail.com>
2022-08-30 16:19:00 -0600

    Cleaned up code

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ const fs = require('fs');
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
-// const { exit } = require('process');
 const generateHTML = require('./utils/generate_html');
 const generateEndHTML = require('./utils/generate_EndHtml')
 const generateManager = require('./utils/generate_manager');
 const generateEngineer = require('./utils/generate_engineer');
-// const generateEmployee = require('./utils/generate_employee');
 const generateIntern = require('./utils/generate_intern');
 const fileName = "index.html";
-
-//Empty Array to contain Employees
-// let employees = [];
 
 // Common Question(s)
 const repeatQuestions = [
@@ -74,9 +68,9 @@ const finalQuestions = [
     }
 ];
 
+//Logic to handle answer from Final Question (Create Engineer, Intern, or Finish)
 async function addInternOrEngineer(questions) {
     await inquirer.prompt(questions).then((answer) => {
-        // console.log(answer)
         if (answer.selection === "Intern") {
             createIntern()
         }
@@ -85,7 +79,6 @@ async function addInternOrEngineer(questions) {
         }
         else if (answer.selection === "Finished Building Team") {
             appendToFile(fileName, generateEndHTML())
-            // exit()
         }
     })
 }
@@ -93,20 +86,8 @@ async function addInternOrEngineer(questions) {
 async function writeToFile(fileName, data) {
     await fs.writeFile(fileName, data, (error) => {
         if (error) throw error;
-        // console.log("Data Saved Successfully");
-
-
     })
 };
-
-// function writeToFile(fileName, data) {
-//     return fs.writeFileSync(path.join(process.cwd(), fileName), data, () => {
-//         if (error) throw error;
-//         console.log("Data Saved Successfully");
-
-
-//     })
-// };
 
 async function appendToFile(fileName, data) {
     await fs.appendFile(fileName, data, (error) => {
@@ -117,56 +98,29 @@ async function appendToFile(fileName, data) {
     })
 };
 
-// function appendToFile(fileName, data) {
-//     return fs.appendFileSync(path.join(process.cwd(), fileName), data, () => {
-//         if (error) throw error;
-//         console.log("Data Saved Successfully");
 
-
-//     })
-// };
-
-// Start with Manager prompt common questions and manager question
-// After manager prompt for select employee from list
+// App will start by calling this function. Asks Manager questions to generate the manager
 async function createManager() {
     const managerQs = [
         ...repeatQuestions,
         ...managerQuestions,
     ];
-    // console.log(managerQs);
     await writeToFile(fileName, generateHTML());
     await inquirer.prompt(managerQs).then((response) => {
-        // employees.push(
-        //     new Manager(
-        //         response.name,
-        //         response.id,
-        //         response.email,
-        //         response.officeNumber,
-        //     )
-        // )
         let managerData = new Manager(response.name, response.id, response.email, response.officeNumber);
         appendToFile(fileName, generateManager(managerData));
         addInternOrEngineer(finalQuestions);
     }
     )
-
 };
 
+// Create Engineer Function
 async function createEngineer() {
     const engineerQs = [
         ...repeatQuestions,
         ...engineerQuestions,
     ];
-    // console.log(engineerQs);
     await inquirer.prompt(engineerQs).then((response) => {
-        // employees.push(
-        //     new Engineer(
-        //         response.name,
-        //         response.id,
-        //         response.email,
-        //         response.gitHubUserName,
-        //     )
-        // )
         let engineerData = new Engineer(response.name, response.id, response.email, response.gitHubUserName)
         appendToFile(fileName, generateEngineer(engineerData));
         addInternOrEngineer(finalQuestions);
@@ -175,45 +129,19 @@ async function createEngineer() {
 
 };
 
+// Create Intern Function
 async function createIntern() {
     const internQs = [
         ...repeatQuestions,
         ...internQuestions,
     ];
-    // console.log(internQs);
     await inquirer.prompt(internQs).then((response) => {
-        // employees.push(
-        //     new Intern(
-        //         response.name,
-        //         response.id,
-        //         response.email,
-        //         response.school,
-        //     )
-        // )
         let internData = new Intern(response.name, response.id, response.email, response.school)
         appendToFile(fileName, generateIntern(internData));
         addInternOrEngineer(finalQuestions);
     }
     )
-
 };
 
-
-
-
+// Calls this function to kick off app
 createManager();
-
-
-
-//Engineer prompt name id email and github
-// than return to list
-
-//intern prompt name id email an github
-// than return to list
-
-//Finish team option
-
-
-//Create a HTML document template
-
-// append with info from employee object
